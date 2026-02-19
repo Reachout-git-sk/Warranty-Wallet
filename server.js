f5a0d52295bd5947f541252a412357e0f72a444e
Sanket_kothari <reachout.sanket@gmail.com>
2026-02-19 17:39:01 -0500

    Fxing server.js with middleware

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const supportRoutes = require("./routes/supportRoutes");
 
 const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Routes
 app.use("/api/purchases", walletRoutes);
@@ -18,7 +23,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
